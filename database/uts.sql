ef14310bb83a821bae91996d4ef97304a0595ece
RAFFI RACHMAWAN <RAFFI RACHMAWAN@DESKTOP-ET2VI37>
2021-11-12 02:12:56 +0700

    started code

--- database/uts.sql
+++ database/uts.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Oct 30, 2021 at 11:35 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.4.20
+-- Host: 127.0.0.1
+-- Generation Time: Nov 11, 2021 at 08:12 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `uts_andi`
+-- Database: `uts`
 --
 
 -- --------------------------------------------------------
@@ -32,15 +32,16 @@ CREATE TABLE `gambar` (
   `gambar1` text NOT NULL,
   `gambar2` text NOT NULL,
   `gambar3` text NOT NULL,
-  `gambar4` text NOT NULL
+  `gambar4` text NOT NULL,
+  `gambar5` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `gambar`
 --
 
-INSERT INTO `gambar` (`id`, `gambar1`, `gambar2`, `gambar3`, `gambar4`) VALUES
-(1, 'https://objectstorage.ap-melbourne-1.oraclecloud.com/p/afVGODqE5mrETmRA_NWPwpMAioJmJHaY0LFXpP80mAuWCNHItIvuxG8X8_nw6Ixu/n/axbuzs1ylays/b/testing_bukcet/o/elonelon.jpg', 'https://objectstorage.ap-melbourne-1.oraclecloud.com/p/afVGODqE5mrETmRA_NWPwpMAioJmJHaY0LFXpP80mAuWCNHItIvuxG8X8_nw6Ixu/n/axbuzs1ylays/b/testing_bukcet/o/elonelon2.jpeg', 'https://objectstorage.ap-melbourne-1.oraclecloud.com/p/afVGODqE5mrETmRA_NWPwpMAioJmJHaY0LFXpP80mAuWCNHItIvuxG8X8_nw6Ixu/n/axbuzs1ylays/b/testing_bukcet/o/elonelon3.jpg\r\n', 'https://objectstorage.ap-melbourne-1.oraclecloud.com/p/afVGODqE5mrETmRA_NWPwpMAioJmJHaY0LFXpP80mAuWCNHItIvuxG8X8_nw6Ixu/n/axbuzs1ylays/b/testing_bukcet/o/elonelon4.jpeg');
+INSERT INTO `gambar` (`id`, `gambar1`, `gambar2`, `gambar3`, `gambar4`, `gambar5`) VALUES
+(1, 'https://objectstorage.ap-tokyo-1.oraclecloud.com/p/Olz0FMJE4ZZtYZzrA7NqPvEyQN4CVVHGSzhWcuP2WoBQnqkqdNIGUygRUvb4XmrN/n/nrtrq7qfkoy6/b/bucket-20211111-1016/o/cu-1.jpg', 'https://objectstorage.ap-tokyo-1.oraclecloud.com/p/Olz0FMJE4ZZtYZzrA7NqPvEyQN4CVVHGSzhWcuP2WoBQnqkqdNIGUygRUvb4XmrN/n/nrtrq7qfkoy6/b/bucket-20211111-1016/o/cu-2.jpg', 'https://objectstorage.ap-tokyo-1.oraclecloud.com/p/Olz0FMJE4ZZtYZzrA7NqPvEyQN4CVVHGSzhWcuP2WoBQnqkqdNIGUygRUvb4XmrN/n/nrtrq7qfkoy6/b/bucket-20211111-1016/o/cu-3.jpg', 'https://objectstorage.ap-tokyo-1.oraclecloud.com/p/Olz0FMJE4ZZtYZzrA7NqPvEyQN4CVVHGSzhWcuP2WoBQnqkqdNIGUygRUvb4XmrN/n/nrtrq7qfkoy6/b/bucket-20211111-1016/o/cu-4.jpg', 'https://objectstorage.ap-tokyo-1.oraclecloud.com/p/Olz0FMJE4ZZtYZzrA7NqPvEyQN4CVVHGSzhWcuP2WoBQnqkqdNIGUygRUvb4XmrN/n/nrtrq7qfkoy6/b/bucket-20211111-1016/o/cu-5.jpg');
 
 --
 -- Indexes for dumped tables
